cb7211247f8192646e782ea463b5f26096bf327c
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-18 11:59:46 -0400

    Successful get/fetch for directors

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -2,6 +2,17 @@ import { useEffect, useState } from "react"
 import NavBar from "../components/NavBar"
 
 function Directors() {
+  const [directors, setDirectors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then((r) => r.json())
+      .then((data) => setDirectors(data))
+      .catch((error) => console.error(error))
+  }, [])
+
+  console.log(directors)
+
   return (
     <>
       <header>
