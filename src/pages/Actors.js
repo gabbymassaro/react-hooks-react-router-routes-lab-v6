a3a5bf2031093567bff5ef7569e9081d19afeef4
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-18 13:34:47 -0400

    Actor's page displays correct data and passes tests

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react"
 import NavBar from "../components/NavBar"
 
 function Actors() {
+  const [actors, setActors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+      .then((r) => r.json())
+      .then((data) => setActors(data))
+      .catch((error) => console.error(error))
+  }, [])
+
   return (
     <>
       <header>
@@ -9,7 +18,16 @@ function Actors() {
       </header>
       <main>
         <h1>Actors Page</h1>
-        {/* Actor info here! */}
+        {actors.map((actor, index) => (
+          <article key={index} name={actor.name}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map((movie, movieIndex) => (
+                <li key={movieIndex}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
       </main>
     </>
   )
